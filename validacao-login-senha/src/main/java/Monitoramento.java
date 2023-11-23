75d10361d8c20b7948293c49961e9b5d41eb8fe6
Vitor Ramos <vitor.mramos@sptech.school>
2023-11-23 15:25:27 -0300

    fix: removendo springboot

--- validacao-login-senha/src/main/java/Monitoramento.java
+++ validacao-login-senha/src/main/java/Monitoramento.java
@@ -7,10 +7,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Monitoramento {
     public static void main(String[] args) {
 
