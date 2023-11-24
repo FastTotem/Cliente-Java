6136d432e788fbe98f8d565a0c2402a1c9f22fb9
vRamosHub <vitor.mramos@sptech.school>
2023-11-23 21:29:27 -0300

    fix: alterando reiniciar e desligar totem

--- validacao-login-senha/src/main/java/funcoes/DesligarTotem.java
+++ validacao-login-senha/src/main/java/funcoes/DesligarTotem.java
@@ -1,5 +1,6 @@
 package funcoes;
 
+import com.github.britooo.looca.api.group.sistema.Sistema;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -10,12 +11,22 @@ public class DesligarTotem implements HttpHandler {
     @Override
     public void handle(HttpExchange t) throws IOException {
         String response = "Desligando totem";
+        Sistema sistema = new Sistema();
 
-        try {
-            Process process = Runtime.getRuntime().exec("shutdown -s -t 3000");
-            process.waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+        if (sistema.getSistemaOperacional().toLowerCase().equals("windows")){
+            try {
+                Process process = Runtime.getRuntime().exec("shutdown -s -t 3000");
+                process.waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else if (sistema.getSistemaOperacional().toLowerCase().equals("linux")){
+            try {
+                Process process = Runtime.getRuntime().exec("sudo shutdown -h now");
+                process.waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         t.sendResponseHeaders(200, response.length());
--- validacao-login-senha/src/main/java/funcoes/ReiniciarTotem.java
+++ validacao-login-senha/src/main/java/funcoes/ReiniciarTotem.java
@@ -1,7 +1,10 @@
 package funcoes;
 
+import com.github.britooo.looca.api.group.sistema.Sistema;
+import com.sun.jna.Platform;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import oshi.SystemInfo;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -10,6 +13,24 @@ public class ReiniciarTotem implements HttpHandler {
     @Override
     public void handle(HttpExchange t) throws IOException {
         String response = "Reiniciando totem";
+        Sistema sistema = new Sistema();
+
+        if (sistema.getSistemaOperacional().toLowerCase().equals("windows")){
+            try {
+                Process process = Runtime.getRuntime().exec("shutdown -r -t 1000");
+                process.waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else if (sistema.getSistemaOperacional().toLowerCase().equals("linux")){
+            try {
+                Process process = Runtime.getRuntime().exec("sudo reboot");
+                process.waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         t.sendResponseHeaders(200, response.length());
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes());
