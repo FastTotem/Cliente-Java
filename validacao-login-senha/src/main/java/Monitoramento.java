00288d8dc60322be854beaefb0ac2161ee4de465
Isabel Alberti <125396002+IsabelCAlberti@users.noreply.github.com>
2023-11-24 19:06:46 -0300

    Deixando codigo mais limpo

--- validacao-login-senha/src/main/java/Monitoramento.java
+++ validacao-login-senha/src/main/java/Monitoramento.java
@@ -1,4 +1,3 @@
-import com.github.britooo.looca.api.group.discos.Disco;
 import com.github.britooo.looca.api.group.dispositivos.DispositivosUsbGrupo;
 import oshi.SystemInfo;
 import slack.FileUploader;
@@ -12,14 +11,12 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Monitoramento {
+    private static Integer idTotem;
     public static void main(String[] args) {
 
+
         Scanner txtScanner = new Scanner(System.in);
-
-        Totem totem = new Totem();
-        String serialNumber = new SystemInfo().getHardware().getComputerSystem().getBaseboard().getSerialNumber();
-        totem.setBoardSerialNumber(serialNumber);
-
+        Logger logger = new Logger();
         DiscosT discosT = new DiscosT();
         MaquinaT maquinaT = new MaquinaT();
         MemoriaT memoriaT = new MemoriaT();
@@ -29,103 +26,96 @@ public class Monitoramento {
         UsbT maquininha = new UsbT(usbs);
         UsbT usbT = new UsbT(usbs);
 
+        Timer timer = new Timer();
+        long delay = 0;
+        long interval = 24 * 60 * 60 * 1000;
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                FileUploader.enviarArquivoParaSlack(logger.getLogFile());
+            }
+        }, delay, interval);
+
+        Totem totem = configurarTotem(txtScanner);
+        if (totem == null) {
+            totem = new Totem();
+            configurarNovoTotem(totem, txtScanner);
+            idTotem = totem.getIdTotem();
+            // Configuração duplicada, removida a segunda chamada de configurarNovoTotem
+        } else {
+            idTotem = totem.getIdTotem();
+            configurarTotemExistente(totem, discosT, memoriaT, processadorT, maquinaT, maquininha);
+        }
+    }
+    private static Totem configurarTotem(Scanner txtScanner) {
+        Totem totem = new Totem();
+        String serialNumber = new SystemInfo().getHardware().getComputerSystem().getBaseboard().getSerialNumber();
+        totem.setBoardSerialNumber(serialNumber);
 
         Mensagens mensagem = new Mensagens();
         System.out.println(mensagem.getBoasVindas());
 
-        totem = totem.validarTotemJaAtivo();
-        Integer idTotem;
+        return totem.validarTotemJaAtivo();
+    }
 
-        if (totem == null) {
+    private static void configurarNovoTotem(Totem totem, Scanner txtScanner) {
+        boolean chaveValida = false;
 
-            Boolean chaveValida = false;
-            totem = new Totem();
+        do {
+            System.out.println("Digite a chave de ativação do totem:");
+            String chaveDeAcesso = txtScanner.nextLine();
 
-            do {
-                System.out.println("Digite a chave de ativação do totem:");
-                String chaveDeAcesso = txtScanner.nextLine();
+            totem.setChaveDeAcesso(chaveDeAcesso);
+            totem = totem.getTotem();
+            if (totem == null) {
+                System.out.println("Chave de ativação incorreta!");
+                totem = new Totem();
+            } else {
+                chaveValida = true;
+            }
 
-                totem.setChaveDeAcesso(chaveDeAcesso);
-                totem = totem.getTotem();
-                if (totem == null) {
-                    System.out.println("Chave de ativação incorreta!");
-                    totem = new Totem();
-                } else {
-                    chaveValida = true;
+        } while (!chaveValida);
+    }
+
+    private static void configurarTotemExistente(Totem totem, DiscosT discosT, MemoriaT memoriaT, ProcessadorT processadorT, MaquinaT maquinaT, UsbT maquininha) {
+        Integer idTotem = totem.getIdTotem();
+
+        discosT.setFkTotem(idTotem);
+        memoriaT.setFkTotem(idTotem);
+        processadorT.setFkTotem(idTotem);
+        maquinaT.setFkTotem(idTotem);
+        maquininha.setFkTotem(idTotem);
+
+        discosT.setIdDiscos();
+        memoriaT.setIdMemoriaTotemValidado();
+        processadorT.setIdProcessadorTotemValidado(idTotem);
+        maquininha.setIdUsbTotemValidado();
+
+        iniciarThreadsDeMonitoramento(processadorT, memoriaT, maquinaT, discosT, maquininha);
+        configurarAgendamentoDeTarefas(processadorT, memoriaT, maquinaT, maquininha);
+    }
+
+    private static void iniciarThreadsDeMonitoramento(ProcessadorT processadorT, MemoriaT memoriaT, MaquinaT maquinaT, DiscosT discosT, UsbT maquininha) {
+        // Inicia threads separadas para monitorar cada componente
+        new Thread(processadorT::monitorarUsoProcessador).start();
+        new Thread(memoriaT::monitorarUsoMemoria).start();
+        new Thread(maquinaT::monitorarTempoAtividade).start();
+        new Thread(maquininha::logUsbDevices).start();
+        new Thread(() -> {
+            try {
+                while (true) {
+                    List<DiscoT> discos = discosT.getDiscosT(); // Obtém a lista de discos
+                    Logger.logDiscoInfo(discos); // Chama o método passando a lista de discos
+                    Thread.sleep(1800000);// Aguarda 2 minutos antes de verificar novamente
                 }
+            } catch (Exception e) {
+                Logger.logInfo("Erro Thread DISCO.\" " + e, Componente.class);
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
-            } while (!chaveValida);
-
-            idTotem = totem.getIdTotem();
-
-            // fkTotem para inserção na captura
-            discosT.setFkTotem(idTotem);
-            memoriaT.setFkTotem(idTotem);
-            processadorT.setFkTotem(idTotem);
-            maquinaT.setFkTotem(idTotem);
-
-            // set id dos componentes para captura
-            memoriaT.setIdComponente(memoriaT.inserirComponente());
-            processadorT.setIdComponente(processadorT.inserirComponente());
-            discosT.inserirDiscos();
-
-            maquinaT.inserirDadosSistema();
-            totem.setBoardSerialNumber(serialNumber);
-            totem.inserirBoardSerialNumber();
-
-            // Encontrando a maquininha
-            maquininha.setMaquininha(cadastroMaquina.cadastrar());
-            maquininha.setFkTotem(totem.getIdTotem());
-            maquininha.inserirDispositivo();
-
-        } else {
-
-            idTotem = totem.getIdTotem();
-
-            // fkTotem para inserção na captura
-            discosT.setFkTotem(idTotem);
-            memoriaT.setFkTotem(idTotem);
-            processadorT.setFkTotem(idTotem);
-            maquinaT.setFkTotem(idTotem);
-            maquininha.setFkTotem(idTotem);
-
-            // set id dos componentes para captura
-            discosT.setIdDiscos();
-            memoriaT.setIdMemoriaTotemValidado();
-            processadorT.setIdProcessadorTotemValidado(idTotem);
-            maquininha.setIdUsbTotemValidado();
-
-            Logger logger = new Logger();
-
-            // Inicia threads separadas para monitorar cada componente
-            new Thread(processadorT::monitorarUsoProcessador).start();
-            new Thread(memoriaT::monitorarUsoMemoria).start();
-            new Thread(maquinaT::monitorarTempoAtividade).start();
-            new Thread(usbT::logUsbDevices).start();
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        List<DiscoT> discos = discosT.getDiscosT(); // Obtém a lista de discos
-                        logger.logDiscoInfo(discos); // Chama o método passando a lista de discos
-                        Thread.sleep(1800000);// Aguarda 2 minutos antes de verificar novamente
-                    }
-                } catch (Exception e) {
-                    Logger.logInfo("Erro Thread DISCO.\" " + e, Componente.class);
-                    e.printStackTrace();
-                }
-            }).start();
-
-            Timer timer = new Timer();
-            long delay = 0;
-            long interval = 24 * 60 * 60 * 1000;
-
-            timer.scheduleAtFixedRate(new TimerTask() {
-                public void run() {
-                    FileUploader.enviarArquivoParaSlack(logger.getLogFile());
-                }
-            }, delay, interval);
-        }
-
+    private static void configurarAgendamentoDeTarefas(ProcessadorT processadorT, MemoriaT memoriaT, MaquinaT maquinaT, UsbT maquininha) {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
 
         scheduler.scheduleAtFixedRate(() -> {
@@ -135,22 +125,12 @@ public class Monitoramento {
             maquininha.logUsbDevices();
             memoriaT.inserirCapturaUsoMemoria();
             processadorT.inserirCapturaUsoProcessador();
-            discosT.inserirCapturasDisco();
-            discosT.inserirReadWrite();
         }, 0, 1, TimeUnit.MINUTES);
 
         scheduler.scheduleAtFixedRate(() -> {
             maquinaT.inserirTempoDeAtividade();
             maquininha.verificarConexao();
         }, 0, 1, TimeUnit.HOURS);
-
-        //execução contínua do código
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            scheduler.shutdown();
-        }
     }
+
 }
